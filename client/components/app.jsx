96c5c6ed50a669e59dd37bf273865b7a1970b335
wayne-daniels <wa.daniels@hotmail.com>
2020-10-07 04:27:29 +0000

    Guest Login Transfers to Welcome Page, userName prop missing

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Login from './login';
 import CreateUsername from './createUsername';
 import Welcome from './welcome';
-import LoginGuest from './loginGuest';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -44,9 +43,6 @@ export default class App extends React.Component {
     }
     if (this.state.view === 'welcome') {
       return <Welcome setView={this.setView} setLocation={this.setLocation} userInfo={this.state.userInfo} locationPrompt={this.locationPrompt} locationPermission={this.state.locationPermission} />;
-    }
-    if (this.state.view === 'loginGuest') {
-      return <LoginGuest setView={this.setView} setLocation={this.setLocation} userInfo={this.state.userInfo} locationPrompt={this.locationPrompt} locationPermission={this.state.locationPermission} />;
     }
   }
 }
